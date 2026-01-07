46f250757d6899b7642b2c5025ae7420ff64fc34
iamdin <iamdinq@gmail.com>
2026-01-08 00:03:28 +0800

    feat: update

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -23,7 +23,7 @@ function App(): React.JSX.Element {
         </div>
         <div className="action">
           <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC 12
+            Send IPC
           </a>
         </div>
       </div>
